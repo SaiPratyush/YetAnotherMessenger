3ec2493c44622959eda4eeb18a31554584192b8d
SaiPratyush Cherukuri <pratyush11235@gmail.com>
2021-01-23 14:44:24 +0530

    added additional events

--- app/events.py
+++ app/events.py
@@ -1,16 +1,60 @@
 """All SocketIO events used by the sever."""
-from flask_socketio import emit
+from flask_socketio import emit, join_room, leave_room
 from . import socketio
 
 
 def handles_events():
     """Print all events handled."""
-    events = ['message', 'connect', 'disconnect']
+    events = ['connect', 'disconnect', 'join', 'leave',
+              'message', 'incoming-msg', 'is-active', 'active-devices']
     print("Handles events: " + ", ".join(events))
 
 
+# Connections
+
+@socketio.on('connect')
+def on_connect():
+    """Handle incomming connections."""
+    print("device connected")
+    emit('connected', "{'message':'device connected'}")
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    """Handle client disconnection."""
+    print("device disconnected")
+
+
+# Rooms
+
+@socketio.on('join')
+def on_join(data):
+    """User joins a room.
+
+    Args:
+        data (JSON): Message sent by client as a JSON object.
+    """
+    room = data["room"]
+    join_room(room)
+    emit('join', "{'message':'joined room'}", room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    """User leaves a room.
+
+    Args:
+        data (JSON): JSON object consisting user id that left the room.
+    """
+    room = data["room"]
+    leave_room(room)
+    print(room+' left the room')
+
+
+# Messages
+
 @socketio.on('message')
-def handle_message(msg):
+def on_message(msg):
     """Handle unnamed messages.
 
     Args:
@@ -20,14 +64,39 @@ def handle_message(msg):
     emit('message', "{'message':'received message'}")
 
 
-@socketio.on('connect')
-def handle_connection():
-    """Handle incomming connections."""
-    print("device connected")
-    emit('connected', "{'message':'device connected'}")
+@socketio.on('incoming-msg')
+def on_incoming_message(data):
+    """Broadcast messages to specific user.
+
+    Args:
+        data (JSON): Message sent by client as a JSON object.
+    """
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    emit('incoming-msg', "{'username': '"+username +
+         "','message':'"+msg+"'}", room=room)
 
 
-@socketio.on('disconnect')
-def handle_disconnection():
-    """Handle client disconnection."""
-    print("device disconnected")
+# Status queries
+
+@socketio.on('is-active')
+def on_is_active(data):
+    """Return active/inactive status of a specific user.
+
+    Args:
+        data (JSON): Data requested by client as a JSON object.
+    """
+    pass
+    # TODO
+
+
+@socketio.on('active-devices')
+def on_active_devices(data):
+    """Return list of devices the user is active on.
+
+    Args:
+        data (JSON): Data requested by client as a JSON object.
+    """
+    pass
+    # TODO
